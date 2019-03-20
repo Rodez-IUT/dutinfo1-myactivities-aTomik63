1bd557dec4457adeca9a97266a28e58931c4ff89
ALEXANDRE BIET <alexandre.biet@iut-rodez.fr>
2019-03-20 17:40:37 +0100

    Fix 4.3 function to fix activities without owner

--- src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
+++ src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
@@ -13,3 +13,16 @@ CREATE OR REPLACE FUNCTION get_default_owner() RETURNS "user" AS $$
 		
 	END
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION fix_activities_without_owner() RETURNS SETOF activity AS $$
+	DECLARE
+		defaultOwner "user"%rowtype;
+	BEGIN
+		defaultOwner := get_default_owner();
+		RETURN QUERY
+		UPDATE activity
+		SET owner_id = defaultOwner.id
+		WHERE owner_id IS NULL
+		RETURNING *;
+	END
+$$ LANGUAGE plpgsql;
